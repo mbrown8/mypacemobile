acb223836bd02e2bdc13ff13cb65d9b0c86dadae
Michael Brown <mbrown8@pace.edu>
2017-09-18 17:05:16 -0400

    init cocoa pods and main view controller testing

--- mypacemobile/ViewController.swift
+++ mypacemobile/ViewController.swift
@@ -7,18 +7,118 @@
 //
 
 import UIKit
+import Material
 
 class ViewController: UIViewController {
-
+    fileprivate var card: ImageCard!
+    
+    /// Conent area.
+    fileprivate var imageView: UIImageView!
+    fileprivate var contentView: UILabel!
+    
+    /// Bottom Bar views.
+    fileprivate var bottomBar: Bar!
+    fileprivate var dateFormatter: DateFormatter!
+    fileprivate var dateLabel: UILabel!
+    fileprivate var favoriteButton: IconButton!
+    fileprivate var shareButton: IconButton!
+    
+    /// Toolbar views.
+    fileprivate var toolbar: Toolbar!
+    fileprivate var moreButton: IconButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        prepareImageView()
+        prepareDateFormatter()
+        prepareDateLabel()
+        prepareFavoriteButton()
+        prepareShareButton()
+        prepareMoreButton()
+        prepareToolbar()
+        prepareContentView()
+        prepareBottomBar()
+        preparePresenterCard()
     }
+}
 
+extension ViewController {
+    fileprivate func prepareImageView() {
+        imageView = UIImageView()
+        imageView.image = UIImage(named: "campus")?.resize(toWidth: view.width)
+    }
+    
+    fileprivate func prepareDateFormatter() {
+        dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+    }
+    fileprivate func prepareDateLabel() {
+        dateLabel = UILabel()
+        dateLabel.font = RobotoFont.regular(with: 12)
+        dateLabel.textColor = Color.blueGrey.base
+        dateLabel.textAlignment = .center
+        dateLabel.text = dateFormatter.string(from: Date.distantFuture)
+    }
+    
+    fileprivate func prepareFavoriteButton() {
+        favoriteButton = IconButton(image: Icon.favorite, tintColor: Color.red.base)
+    }
+    
+    fileprivate func prepareShareButton() {
+        shareButton = IconButton(image: Icon.cm.share, tintColor: Color.blueGrey.base)
+    }
+    
+    fileprivate func prepareMoreButton() {
+        moreButton = IconButton(image: Icon.cm.moreHorizontal, tintColor: Color.blueGrey.base)
+    }
+    
+    fileprivate func prepareToolbar() {
+        toolbar = Toolbar(rightViews: [moreButton])
+        toolbar.backgroundColor = nil
+        
+        toolbar.title = "MyPaceMobile"
+        toolbar.titleLabel.textColor = .white
+        toolbar.titleLabel.textAlignment = .center
+        
+        toolbar.detail = "The New application update"
+        toolbar.detailLabel.textColor = .white
+        toolbar.detailLabel.textAlignment = .center
+    }
+    
+    fileprivate func prepareContentView() {
+        contentView = UILabel()
+        contentView.numberOfLines = 0
+        contentView.text = "Pace University MyPaceMobile"
+        contentView.font = RobotoFont.regular(with: 14)
+    }
+    
+    fileprivate func prepareBottomBar() {
+        bottomBar = Bar(leftViews: [favoriteButton], rightViews: [shareButton], centerViews: [dateLabel])
+    }
+    
+    fileprivate func preparePresenterCard() {
+        card = ImageCard()
+        
+        card.toolbar = toolbar
+        card.toolbarEdgeInsetsPreset = .square3
+        card.imageView = imageView
+        card.contentView = contentView
+        card.contentViewEdgeInsetsPreset = .square3
+        card.bottomBar = bottomBar
+        card.bottomBarEdgeInsetsPreset = .wideRectangle2
+        card.layer.cornerRadius = 1
+        
+        view.layout(card).horizontally(left: 20, right: 20).center()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+
 
 
 }
